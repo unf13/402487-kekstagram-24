a8e3a57259fce9efa6b6b47a7256ba7d2c2b210a
Paul Bekker <unf13@mail.ru>
2021-10-21 14:37:09 +0700

    "домашнее задание 5.12. Модуляция"

---
+++ js/photo-description-generation.js
@@ -0,0 +1,35 @@
+import {getRandomInteger,generateRandomId} from './utils.js';
+import {DESCRIPTIONS_TEXTS,MESSAGES,NAMES} from './data.js';
+
+const createComment = () => {
+  const randomMessageIndex = getRandomInteger(0,MESSAGES.length-1);
+  const randomNameIndex = getRandomInteger(0,NAMES.length-1);
+  const randomAvatarIndex = getRandomInteger(0,NAMES.length-1);
+  const randomID = generateRandomId();
+  return {
+    id: randomID,
+    avatar: `img/avatar-${randomAvatarIndex}.svg`,
+    message: MESSAGES[randomMessageIndex],
+    name: NAMES[randomNameIndex],
+  };
+};
+
+const createPhotoDescription = (id) => {
+  const randomDescriptionIndex = getRandomInteger(0,DESCRIPTIONS_TEXTS.length-1);
+  const commentsList = Array.from({length:id},createComment);// пусть количество комментариев будет, допустим, равно id
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: DESCRIPTIONS_TEXTS[randomDescriptionIndex],
+    likes: getRandomInteger(15,200),
+    comments: commentsList,
+  };
+};
+
+const generatePhotoDescriptions = () => {
+  for (let id = 1; id <= 25; id++) {
+    createPhotoDescription(id);
+  }
+};
+
+export {generatePhotoDescriptions};
---
+++ js/utils.js
@@ -0,0 +1,17 @@
+/* source - https://learn.javascript.ru/task/random-int-min-max */
+const getRandomInteger = (min, max) => {
+
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  if (max <= min) {
+    return min;
+  }
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+const checkStringLength = (stringToCheck, maxLength) => stringToCheck.length <= maxLength;
+
+const generateRandomId = () => getRandomInteger(0,1000) + new Date().getTime();
+
+export {getRandomInteger,checkStringLength,generateRandomId};
